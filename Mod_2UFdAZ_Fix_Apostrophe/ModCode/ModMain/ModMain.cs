9f7d1dafb073014c06dc19ffdef9b2ff8829e79c
nozwock <57829219+nozwock@users.noreply.github.com>
2025-08-28 20:14:33 +0530

    Add mod 'Fix Apostrophe'
    A band-aid fix for the game rendering curly-apostrophes with a weird
    empty gap.

---
+++ Mod_2UFdAZ_Fix_Apostrophe/ModCode/ModMain/ModMain.cs
@@ -0,0 +1,28 @@
+﻿namespace MOD_2UFdAZ
+{
+    public class ModMain
+    {
+        public void Init()
+        {
+            foreach (var item in g.conf.localText.allConfList)
+            {
+                item.ch = item.ch?.Replace("’", "'");
+                item.tc = item.tc?.Replace("’", "'");
+                item.en = item.en?.Replace("’", "'");
+                item.kr = item.kr?.Replace("’", "'");
+            }
+
+            foreach (var item in g.conf.roleLogLocal.allConfList)
+            {
+                item.ch = item.ch?.Replace("’", "'");
+                item.tc = item.tc?.Replace("’", "'");
+                item.en = item.en?.Replace("’", "'");
+                item.kr = item.kr?.Replace("’", "'");
+            }
+        }
+
+        public void Destroy()
+        {
+        }
+    }
+}
