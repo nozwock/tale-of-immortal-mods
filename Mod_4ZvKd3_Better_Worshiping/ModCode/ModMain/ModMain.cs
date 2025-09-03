b11ed2ecada4ad288d6fdebed9f582b0829b26c5
nozwock <57829219+nozwock@users.noreply.github.com>
2025-09-03 19:33:40 +0530

    Add mod 'Better Worshiping'
    For letting the player keep worshiping until they've filled up the bar
    once that month. It makes things a bit nicer as you no longer have to
    guess what and how much to offer in order to fill up the bar, you can
    just keep offering until the bar is full now.

---
+++ Mod_4ZvKd3_Better_Worshiping/ModCode/ModMain/ModMain.cs
@@ -0,0 +1,55 @@
+using EGameTypeData;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace MOD_4ZvKd3
+{
+    public class ModMain
+    {
+        public void Init()
+        {
+            g.events.On(EGameType.OpenUIEnd, (System.Action<ETypeData>)OnOpenUIEnd);
+        }
+
+        public void Destroy()
+        {
+            g.events.Off(EGameType.OpenUIEnd, (System.Action<ETypeData>)OnOpenUIEnd);
+        }
+
+        private void OnOpenUIEnd(ETypeData e)
+        {
+            if (e.Cast<OpenUIEnd>().uiType.uiName != UIType.ImmortalAncestralHall.uiName)
+            {
+                return;
+            }
+
+            var ui = g.ui.GetUI<UIImmortalAncestralHall>(UIType.ImmortalAncestralHall);
+            if (ui == null)
+            {
+                return;
+            }
+
+            // MapBuild10008
+            //     MapBuild10008.Data data
+            //     MapBuild10008Data build10008Data
+            //         ConfWorldBuilding10008Item building10008Item
+            //         MapBuild10008 build10008
+
+            var buildData = ui.build10008Data.build10008.data;
+            var dataItem = ui.build10008Data.building10008Item;
+
+            var originalCb = ui.btnChallenge.onClick;
+            ui.btnChallenge.onClick = new Button.ButtonClickedEvent();
+            ui.btnChallenge.onClick.AddListener((UnityAction)delegate
+            {
+                var isBlessingReceived = buildData.giveValue == 0 && buildData.lastGiveMonth != 0;
+                if (buildData.giveValue < dataItem.consecrateAmount && !isBlessingReceived)
+                {
+                    buildData.lastGiveMonth = 0; // Allow worship
+                }
+
+                originalCb?.Invoke();
+            });
+        }
+    }
+}
