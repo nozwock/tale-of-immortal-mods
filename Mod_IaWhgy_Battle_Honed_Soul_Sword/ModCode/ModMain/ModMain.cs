b4437f7b33257eba557bcf801261c5bf5224def0
nozwock <57829219+nozwock@users.noreply.github.com>
2025-09-05 06:52:54 +0530

    Add 'Devour Deadline Mode' to config with ResetOnBeastDevour as default
    There are options to completely disable the deadline system, to just
    reset the deadline on defeating a mythical beast or to not change
    anything.

--- Mod_IaWhgy_Battle_Honed_Soul_Sword/ModCode/ModMain/ModMain.cs
+++ Mod_IaWhgy_Battle_Honed_Soul_Sword/ModCode/ModMain/ModMain.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Reflection;
+using System.ComponentModel;
 using System.Collections.Generic;
 using EBattleTypeData;
 using MelonLoader;
 using System.Linq;
 using System.Text;
+using EGameTypeData;
 
 namespace MOD_IaWhgy
 {
@@ -16,9 +19,34 @@ namespace MOD_IaWhgy
         private int? soulDevourSwordAtkLimit;
         private int bossKillCount;
 
+        private int newMonth;
+
         private MelonPreferences_Category category;
         private MelonPreferences_Entry<double> mythicalBeastDevourFraction;
         private MelonPreferences_Entry<bool> showNotification;
+        private static MelonPreferences_Entry<string> devourDeadlineMode;
+
+        private DevourDeadlineMode CurrentDevourDeadlineMode =>
+            Enum.TryParse<DevourDeadlineMode>(devourDeadlineMode.Value, out var parsed) ? parsed : DevourDeadlineMode.ResetOnBeastDevour;
+
+        private int CurrentMonth
+        {
+            get
+            {
+                var date = GameTool.DayToDate(g.data.world.worldDay);
+                return (date[0] - 1) * 12 + date[1];
+            }
+        }
+
+        private enum DevourDeadlineMode
+        {
+            [Description("Disable deadline system completely")]
+            Disabled,
+            [Description("Reset deadline when devouring beasts")]
+            ResetOnBeastDevour,
+            [Description("Leave deadline system unchanged")]
+            Unchanged
+        }
 
         public void Init()
         {
@@ -33,10 +61,16 @@ namespace MOD_IaWhgy
                 true,
                 description: "Should notification for added Soul Reaver stats be shown at the end of the battle?"
             );
+            devourDeadlineMode = category.CreateEntry(
+                "Devour Deadline Mode",
+                DevourDeadlineMode.ResetOnBeastDevour.ToString(),
+                description: GetEnumDescription<DevourDeadlineMode>()
+            );
 
             g.events.On(EBattleType.BattleStart, (Il2CppSystem.Action<ETypeData>)OnBattleStart);
             g.events.On(EBattleType.BattleEnd, (Il2CppSystem.Action<ETypeData>)OnBattleEnd);
             g.events.On(EBattleType.UnitDie, (Il2CppSystem.Action<ETypeData>)OnUnitDie);
+            g.events.On(EGameType.OpenUIEnd, (Il2CppSystem.Action<ETypeData>)OnOpenUIEnd);
         }
 
         public void Destroy()
@@ -44,6 +78,36 @@ namespace MOD_IaWhgy
             g.events.Off(EBattleType.BattleStart, (Il2CppSystem.Action<ETypeData>)OnBattleStart);
             g.events.Off(EBattleType.BattleEnd, (Il2CppSystem.Action<ETypeData>)OnBattleEnd);
             g.events.Off(EBattleType.UnitDie, (Il2CppSystem.Action<ETypeData>)OnUnitDie);
+            g.events.Off(EGameType.OpenUIEnd, (Il2CppSystem.Action<ETypeData>)OnOpenUIEnd);
+        }
+
+        private void OnOpenUIEnd(ETypeData e)
+        {
+            var edata = e.Cast<OpenUIEnd>();
+            if (edata.uiType.uiName == UIType.LoadingBar.uiName) // The circular loading UI on month skip
+            {
+                if (newMonth != CurrentMonth)
+                {
+                    OnSkipMonth();
+                    newMonth = CurrentMonth;
+                }
+            }
+        }
+
+        private void OnSkipMonth()
+        {
+            if (CurrentDevourDeadlineMode == DevourDeadlineMode.Disabled)
+            {
+                var swordMgr = g.world.soulDevourSword;
+
+                var playerUnitId = g.world.playerUnit.data.unitData.unitID;
+                if (swordMgr.data.unitId != playerUnitId)
+                {
+                    return;
+                }
+
+                SetSoulSwordDevourMonth(CurrentMonth); // Keep resetting deadline every month
+            }
         }
 
         private void OnUnitDie(ETypeData e)
@@ -85,6 +149,11 @@ namespace MOD_IaWhgy
                 return;
             }
 
+            if (CurrentDevourDeadlineMode == DevourDeadlineMode.ResetOnBeastDevour)
+            {
+                SetSoulSwordDevourMonth(CurrentMonth);
+            }
+
             var confList_ = g.conf.soulDevourSwordDevour._allConfList;
             var confList = new List<ConfSoulDevourSwordDevourItem>();
             for (int i = 0; i < Math.Min(confList_.Count, dungeonGrade); i++)
@@ -114,6 +183,18 @@ namespace MOD_IaWhgy
                 }
                 UITipItem.AddTip(tip.ToString(), 2f);
             }
+        }
+
+        /// <summary>
+        /// Preserves cooldown state.
+        /// </summary>
+        private static void SetSoulSwordDevourMonth(int month)
+        {
+            var swordMgr = g.world.soulDevourSword;
+            // Keeping the cooldown as-is
+            swordMgr.data.nextDevourMonth = month - 1 + Math.Abs(swordMgr.data.nextDevourMonth - swordMgr.data.lastDevourMonth);
+            // Assuming month here is starting with 0
+            swordMgr.data.lastDevourMonth = month - 1;
         }
 
         private void AddSoulSwordAtk(int addAtk)
@@ -150,6 +231,25 @@ namespace MOD_IaWhgy
                     seq[i] = seq[i - 1] + 1;
                 }
             }
+        }
+
+        private static string GetEnumDescription<T>(string sep = "\n") where T : Enum
+        {
+            return string.Join(sep,
+                Enum.GetValues(typeof(T))
+                    .Cast<T>()
+                    .Select(v =>
+                    {
+                        var memInfo = typeof(T).GetMember(v.ToString()).FirstOrDefault();
+                        var attr = memInfo?
+                            .GetCustomAttributes(typeof(DescriptionAttribute), false)
+                            .Cast<DescriptionAttribute>()
+                            .FirstOrDefault();
+
+                        return attr != null
+                            ? $"{v} ({attr.Description})"
+                            : v.ToString();
+                    }));
         }
 
         private static void Log(object s)
