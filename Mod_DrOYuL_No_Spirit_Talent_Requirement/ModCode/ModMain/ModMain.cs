d2bab715dfe38b8825195d43f60ef70504387000
nozwock <57829219+nozwock@users.noreply.github.com>
2025-09-30 21:42:19 +0530

    Add mod to remove artifact spirits' special talent requirements

---
+++ Mod_DrOYuL_No_Spirit_Talent_Requirement/ModCode/ModMain/ModMain.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using MelonLoader;
+using UnhollowerBaseLib;
+
+namespace MOD_DrOYuL;
+
+public class ModMain
+{
+	internal static readonly string modNamespace = typeof(ModMain).Namespace!;
+
+	Il2CppSystem.Action<ETypeData> callIntoWorld;
+
+	// static readonly Regex DewIdPat = new(@"^5311\d201$", RegexOptions.Compiled);
+
+	public void Init()
+	{
+		callIntoWorld = (Il2CppSystem.Action<ETypeData>)OnIntoWorld;
+		g.events.On(EGameType.IntoWorld, callIntoWorld);
+	}
+
+	public void Destroy()
+	{
+		g.events.Off(EGameType.IntoWorld, callIntoWorld);
+	}
+
+	void OnIntoWorld(ETypeData e)
+	{
+		// var dews = new Dictionary<int, int>();
+		// foreach (var conf in g.conf.artifactSpriteTalent._allConfList)
+		// {
+		// 	if (
+		// 		!dews.ContainsKey(conf.spriteID)
+		// 		&& conf.activeCost.Length >= 1
+		// 		&& conf.activeCost[0].Length >= 2
+		// 		&& DewIdPat.IsMatch(conf.activeCost[0][0].ToString())
+		// 	)
+		// 	{
+		// 		dews[conf.spriteID] = conf.activeCost[0][0];
+		// 	}
+		// }
+
+		foreach (var conf in g.conf.artifactSpriteTalent._allConfList)
+		{
+			if (conf.unlock2Type != 0 && conf.unlock2Type != 17 /* No Cost */)
+			{
+				conf.unlock2Type = 17;
+				conf.unlock2Value = conf.axisX.ToString();
+				conf.unlock2Count = 1;
+				conf.unlock3Type = 0;
+				conf.unlock3Value = "0";
+				conf.unlock3Count = 0;
+				conf.unlockDesc = "0";
+				conf.unlockDesc = "spriteTalent_unlockDesc100522"; // No Cost
+
+				// var outer = new Il2CppReferenceArray<Il2CppStructArray<int>>(1);
+				// var inner = new Il2CppStructArray<int>(2);
+				// inner[0] = dews[conf.spriteID];
+				// inner[1] = conf.addSoul;
+				// outer[0] = inner;
+				// conf.activeCost = outer;
+			}
+		}
+	}
+
+	internal static void Log(object s)
+	{
+		MelonLogger.Msg($"{modNamespace}: {s}");
+	}
+}
