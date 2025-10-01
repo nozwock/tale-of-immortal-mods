ee7cad53e40743d512f633c0f5d0ad1cb2a4bff2
nozwock <57829219+nozwock@users.noreply.github.com>
2025-10-01 19:11:10 +0530

    Allow unlocking already unlocked spirit talents at no cost after reset
    This can be done by modifying the talent requirements configuration type
    for the spirit talents that are known to have been unlocked at least
    once.

--- Mod_OqiZtz_Reset_Spirit_Talents/ModCode/ModMain/ModMain.cs
+++ Mod_OqiZtz_Reset_Spirit_Talents/ModCode/ModMain/ModMain.cs
@@ -4,6 +4,9 @@ using EGameTypeData;
 using MelonLoader;
 using UnityEngine.Events;
 using UnityEngine;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnhollowerBaseLib;
 
 namespace MOD_OqiZtz;
 
@@ -11,17 +14,104 @@ public class ModMain
 {
 	internal static readonly string modNamespace = typeof(ModMain).Namespace!;
 
+	Il2CppSystem.Action<ETypeData> callSaveData;
+	Il2CppSystem.Action<ETypeData> callIntoWorld;
 	Il2CppSystem.Action<ETypeData> callOpenUIEnd;
+
+	public static Dictionary<int, HashSet<int>> unlockedSpiritTalents = [];
+
+	public enum SaveObjKey
+	{
+		UnlockedSpiritTalents
+	}
 
 	public void Init()
 	{
+		callIntoWorld = (Il2CppSystem.Action<ETypeData>)OnIntoWorld;
+		callSaveData = (Il2CppSystem.Action<ETypeData>)OnSaveData;
 		callOpenUIEnd = (Il2CppSystem.Action<ETypeData>)OnOpenUIEnd;
+		g.events.On(EGameType.IntoWorld, callIntoWorld);
+		g.events.On(EGameType.SaveData, callSaveData);
 		g.events.On(EGameType.OpenUIEnd, callOpenUIEnd);
 	}
 
 	public void Destroy()
 	{
+		g.events.Off(EGameType.IntoWorld, callIntoWorld);
+		g.events.Off(EGameType.SaveData, callSaveData);
 		g.events.Off(EGameType.OpenUIEnd, callOpenUIEnd);
+	}
+
+	void OnIntoWorld(ETypeData _)
+	{
+		try
+		{
+			if (g.data.obj.ContainsKey(modNamespace, SaveObjKey.UnlockedSpiritTalents.ToString()))
+			{
+				var json = g.data.obj.GetString(modNamespace, SaveObjKey.UnlockedSpiritTalents.ToString());
+				unlockedSpiritTalents = JsonConvert.DeserializeObject<Dictionary<int, HashSet<int>>>(json);
+			}
+			else // Loaded a save that doesn't have it
+			{
+				unlockedSpiritTalents.Clear();
+			}
+
+			SetNoCostTalentForUnlocked();
+		}
+		catch (Exception ex)
+		{
+			Log(ex);
+		}
+	}
+
+	void OnSaveData(ETypeData _)
+	{
+		try
+		{
+			var json = JsonConvert.SerializeObject(unlockedSpiritTalents);
+			g.data.obj.SetString(modNamespace, SaveObjKey.UnlockedSpiritTalents.ToString(), json);
+		}
+		catch (Exception ex)
+		{
+			Log(ex);
+		}
+	}
+
+	static void SetNoCostTalentForUnlocked(int spriteId = -1)
+	{
+		HashSet<int> unlockedTalents;
+		foreach (var conf in g.conf.artifactSpriteTalent._allConfList)
+		{
+			if (spriteId != -1 && spriteId != conf.spriteID) // Only modify talent for this spirit
+				continue;
+			if (unlockedSpiritTalents.TryGetValue(conf.spriteID, out unlockedTalents) && unlockedTalents.Contains(conf.number) && conf.unlock2Type != 17 /*No Cost*/)
+			{
+				conf.unlock2Type = 17;
+				conf.unlock2Value = conf.axisX.ToString();
+				conf.unlock2Count = 1;
+				conf.unlock3Type = 0;
+				conf.unlock3Value = "0";
+				conf.unlock3Count = 0;
+				conf.unlockDesc = "0";
+				conf.unlockDesc = "spriteTalent_unlockDesc100522"; // No Cost
+				conf.activeCost = new Il2CppReferenceArray<Il2CppStructArray<int>>(0);
+			}
+		}
+
+		foreach (var sprite in g.world.playerUnit.data.unitData.artifactSpriteData.sprites)
+		{
+			if (spriteId != -1 && spriteId != sprite.spriteID)
+				continue;
+			foreach (var talent in sprite.talents)
+			{
+				if (unlockedSpiritTalents.TryGetValue(sprite.spriteID, out unlockedTalents) && unlockedTalents.Contains(talent.number))
+				{
+					// Necessary, otherwise the discrepancy will result in a "No Cost" popup by game on trying to unlock
+					talent.unlock2Count = 1;
+					talent.unlock3Count = 0;
+				}
+			}
+		}
 	}
 
 	void OnOpenUIEnd(ETypeData e)
@@ -69,16 +159,22 @@ public class ModMain
 								{
 									foreach (var talent in sprite.talents)
 									{
+										if (talent.state == 1)
+										{
+											if (!unlockedSpiritTalents.TryGetValue(sprite.spriteID, out var set))
+											{
+												set = [];
+												unlockedSpiritTalents[sprite.spriteID] = set;
+											}
+											set.Add(talent.number);
+										}
 										talent.state = 0;
 									}
 									break;
 								}
 							}
 
-							// FIXME: Talent progress towards activeCost doesn't get preserved anywhere.
-							// Will need to keep track of unlocked talents in save file separtaly via: 
-							// g.data.obj.SetString(group, key, value)
-							// And then modify the ConfArtifactSpriteTalent to make those already unlocked as "No Cost"
+							SetNoCostTalentForUnlocked(ui.uiSprite.spriteList[i].spriteID);
 
 							// Update UI
 							var toggle = ui.uiSprite.goSpriteItemRoot.transform.GetChild(i).GetComponentInChildren<Toggle>();
