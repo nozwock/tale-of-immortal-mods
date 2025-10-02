dcf4ed8b8db16d9f1d4aefb73fa330e334aa6636
nozwock <57829219+nozwock@users.noreply.github.com>
2025-10-02 20:23:38 +0530

    Fix clipped skill names in Greenscale Spring UI when English is set

--- Mod_7tLNX4_Fixes/ModCode/ModMain/ModMain.cs
+++ Mod_7tLNX4_Fixes/ModCode/ModMain/ModMain.cs
@@ -1,6 +1,7 @@
 using MelonLoader;
 using EGameTypeData;
 using UnityEngine.UI;
+using UnityEngine;
 
 namespace MOD_7tLNX4
 {
@@ -20,6 +21,19 @@ namespace MOD_7tLNX4
 		public void Destroy()
 		{
 			g.events.Off(EGameType.OpenUIEnd, callOpenUIEnd);
+		}
+
+		static void FixDragonDoorUpgradeCutSkillText(UIBase _ui)
+		{
+			var ui = _ui.GetComponent<UIDragonDoorUpgrade>();
+			for (int i = 0; i < ui.goRoot.transform.childCount; i++)
+			{
+				var tItem = ui.goRoot.transform.GetChild(i);
+				if (!tItem.Find("bg").gameObject.active)
+					continue;
+				var skillName = tItem.Find("Name").GetComponent<Text>();
+				skillName.horizontalOverflow = HorizontalWrapMode.Overflow;
+			}
 		}
 
 		static void FixSoulReaverEngDescription(UIBase ui)
@@ -44,6 +58,10 @@ namespace MOD_7tLNX4
 			if (edata.uiType.uiName == UIType.PropInfoSoulDevourSword.uiName)
 			{
 				FixSoulReaverEngDescription(edata.ui);
+			}
+			else if (edata.uiType.uiName == UIType.DragonDoorUpgrade.uiName)
+			{
+				FixDragonDoorUpgradeCutSkillText(edata.ui);
 			}
 		}
 
